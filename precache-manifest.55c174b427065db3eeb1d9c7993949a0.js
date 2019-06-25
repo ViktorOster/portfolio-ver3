59f76194843e2c5bb9c0ddc9d9615a3512c6ccbf
ViktorOster <Ozterkvlt@hotmail.com>
2019-06-25 13:32:57 +0300

    Updates

---
+++ precache-manifest.55c174b427065db3eeb1d9c7993949a0.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd54e79e7d8a3a5413130cd463833691",
+    "url": "/portfolio-ver3/index.html"
+  },
+  {
+    "revision": "b49cbcc39866603cde7e",
+    "url": "/portfolio-ver3/static/css/main.1a04f05d.chunk.css"
+  },
+  {
+    "revision": "6bb75b3c6c306ed265f4",
+    "url": "/portfolio-ver3/static/js/2.df613710.chunk.js"
+  },
+  {
+    "revision": "b49cbcc39866603cde7e",
+    "url": "/portfolio-ver3/static/js/main.53c0ea44.chunk.js"
+  },
+  {
+    "revision": "8ca464a41bcf64d9c260",
+    "url": "/portfolio-ver3/static/js/runtime~main.86b108bb.js"
+  },
+  {
+    "revision": "fc36c747139ed18e4c12ccd334e173fe",
+    "url": "/portfolio-ver3/static/media/Mockup2.fc36c747.png"
+  },
+  {
+    "revision": "46dbc9b3aeee0791bdff2e72c5b45e38",
+    "url": "/portfolio-ver3/static/media/Mockup2_nomobile.46dbc9b3.png"
+  },
+  {
+    "revision": "4e34352d5de90b06b9d7c1073a2767d2",
+    "url": "/portfolio-ver3/static/media/SPMS_laptop.4e34352d.png"
+  },
+  {
+    "revision": "91832372e8f02f34c2479f45524e3f4b",
+    "url": "/portfolio-ver3/static/media/avatar illustration transparentB simp.91832372.svg"
+  },
+  {
+    "revision": "621aaf253cec1d41643c0dd3f1e5cde2",
+    "url": "/portfolio-ver3/static/media/baseline-code-color.621aaf25.svg"
+  },
+  {
+    "revision": "d9f2b78148792d1ab7a5f5ff51d3146f",
+    "url": "/portfolio-ver3/static/media/baseline-code.d9f2b781.svg"
+  },
+  {
+    "revision": "239378c755faba393e0d4cf7d9f0a527",
+    "url": "/portfolio-ver3/static/media/eAuction_laptop.239378c7.png"
+  },
+  {
+    "revision": "64341e3ffefd0de44e84a15a51e2478d",
+    "url": "/portfolio-ver3/static/media/iconfinder_globe-color.64341e3f.svg"
+  },
+  {
+    "revision": "f8f7550bd9f14fe4532face2f44133fe",
+    "url": "/portfolio-ver3/static/media/iconfinder_globe.f8f7550b.svg"
+  },
+  {
+    "revision": "4fe2cfaef9d68ccd2f2e1b9e54d880b7",
+    "url": "/portfolio-ver3/static/media/icons8-delete.4fe2cfae.svg"
+  },
+  {
+    "revision": "f01f024b563eda7dab69d6b0c44ad82b",
+    "url": "/portfolio-ver3/static/media/icons8-download-from-the-cloud.f01f024b.svg"
+  },
+  {
+    "revision": "afc2066386134feb356fca156c4fcc2e",
+    "url": "/portfolio-ver3/static/media/icons8-github.afc20663.svg"
+  },
+  {
+    "revision": "de928acad08754de711c7068eff0f60f",
+    "url": "/portfolio-ver3/static/media/icons8-linkedin.de928aca.svg"
+  },
+  {
+    "revision": "c6363a148be96cc6c0738a22c177427a",
+    "url": "/portfolio-ver3/static/media/icons8-menu.c6363a14.svg"
+  },
+  {
+    "revision": "0b648348ab1399cf918625d03493283d",
+    "url": "/portfolio-ver3/static/media/logo personal.0b648348.svg"
+  },
+  {
+    "revision": "0b9235c3b9875ba7995fef1d5b4b287a",
+    "url": "/portfolio-ver3/static/media/placeholder-profile-sq.0b9235c3.jpg"
+  },
+  {
+    "revision": "1c8f731e28d29d1ac411a65ec3060f1b",
+    "url": "/portfolio-ver3/static/media/wavesynth_laptop.1c8f731e.png"
+  }
+]);
